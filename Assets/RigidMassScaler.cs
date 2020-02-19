9ce1140d73fc24a1f0bff32cb90ed181028f8be6
Karolis Uzkuraitis <karolis.uzkuraitis+github@gmail.com>
2020-02-19 21:11:30 +0200

    Messing with bot character mass

---
+++ Assets/RigidMassScaler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidMassScaler : MonoBehaviour
+{
+    void Start()
+    {
+        foreach (var rb in GetComponentsInChildren<Rigidbody>())
+        {
+            rb.mass = rb.mass * transform.lossyScale.x;
+        }
+    }
+}
