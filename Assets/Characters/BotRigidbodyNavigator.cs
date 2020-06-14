bbfddb979aee3bdb989186a340a26382b8164af4
Karolis Uzkuraitis <karolis.uzkuraitis+github@gmail.com>
2020-06-14 09:31:19 +0300

    formatting

--- Assets/Characters/BotRigidbodyNavigator.cs
+++ Assets/Characters/BotRigidbodyNavigator.cs
@@ -134,11 +134,11 @@ public class BotRigidbodyNavigator : MonoBehaviour
          NavMeshTriangulation navMeshData = NavMesh.CalculateTriangulation();
  
          // Pick the first indice of a random triangle in the nav mesh
-         int t = Random.Range(0, navMeshData.indices.Length-3);
+        int t = Random.Range(0, navMeshData.indices.Length - 3);
          
          // Select a random point on it
-         Vector3 point = Vector3.Lerp(navMeshData.vertices[navMeshData.indices[t]], navMeshData.vertices[navMeshData.indices[t+1]], Random.value);
-         Vector3.Lerp(point, navMeshData.vertices[navMeshData.indices[t+2]], Random.value);
+        Vector3 point = Vector3.Lerp(navMeshData.vertices[navMeshData.indices[t]], navMeshData.vertices[navMeshData.indices[t + 1]], Random.value);
+        Vector3.Lerp(point, navMeshData.vertices[navMeshData.indices[t + 2]], Random.value);
  
          return point;
      }
