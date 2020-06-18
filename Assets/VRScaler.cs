b03cd5f685bca0ac05380f34e9edd712ebfbcd3e
Karolis Uzkuraitis <karolis.uzkuraitis+github@gmail.com>
2020-06-18 20:41:10 +0300

    🎆🎉 Finally SteamVR scaler workaround Finally managed to get SteamVR rig scaling properly. It was as simple as scaling during runtime only. I guess they have some scaling code initialization requiring 1x1x1 scale

---
+++ Assets/VRScaler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRScaler : MonoBehaviour
+{
+    [SerializeField] Vector3 playerScale = new Vector3(1, 1, 1);
+
+    private bool scaleSet = false;
+
+    void Update()
+    {
+        // If I update scale before launching, hands get all messed up and offset.
+        // Even Start() is too late for that.
+        // Works if updated during first frame. My guess, some initialization that required for scale to be 1x1x1 is run in start of some SteamVR script.
+        if (!scaleSet)
+        {
+            transform.localScale = playerScale;
+            scaleSet = true;
+            // Even destroy this script so it doesn't get ticket anymore.
+            Destroy(this);
+        }
+    }
+}
